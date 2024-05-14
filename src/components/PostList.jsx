2ec2503d86d93b7e9a5df910c26d47e3bdfc7cef
Rafal <kuna240@gmail.com>
2024-05-14 21:16:43 +0100

    modal

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import classes from "./PostList.module.css";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 const PostList = () => {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
@@ -17,10 +18,13 @@ const PostList = () => {
 
   return (
     <>
-      <NewPost
-        onBodyChangeHandler={bodyChangeHandler}
-        onAuthorChangeHandler={authorChangeHandler}
-      />
+      <Modal>
+        <NewPost
+          onBodyChangeHandler={bodyChangeHandler}
+          onAuthorChangeHandler={authorChangeHandler}
+        />
+      </Modal>
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Rafal" body="Finally I will get my first job in It" />
