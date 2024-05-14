453a98160a0494d0e0dd742b9dce95139879d540
Rafal <kuna240@gmail.com>
2024-05-14 20:47:30 +0100

    state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import classes from "./PostList.module.css";
 import Post from "./Post";
+import NewPost from "./NewPost";
 const PostList = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    event.preventDefault();
+    setEnteredBody(event.target.value);
+  };
+  const authorChangeHandler = (event) => {
+    event.preventDefault();
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Rafal" body="Finally I will get my first job in It" />
-      <Post author="Rafal" body="Junior React Developer" />
-    </ul>
+    <>
+      <NewPost
+        onBodyChangeHandler={bodyChangeHandler}
+        onAuthorChangeHandler={authorChangeHandler}
+      />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Rafal" body="Finally I will get my first job in It" />
+        <Post author="Rafal" body="Vamos Barca !!!" />
+      </ul>
+    </>
   );
 };
 export default PostList;
