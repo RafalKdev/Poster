212103bda152aab051cd7e432a96945bf0210b57
Rafal <kuna240@gmail.com>
2024-05-15 20:41:47 +0100

    conditionalContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import MainHeader from "./components/MainHeader";
 import PostList from "./components/PostList";
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
+  const showModalHandler = () => {
+    setModalIsVisible(true);
+  };
+
   return (
     <>
+      <MainHeader onCreatePost={showModalHandler} />
       <main>
-        <PostList />
+        <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
       </main>
     </>
   );
